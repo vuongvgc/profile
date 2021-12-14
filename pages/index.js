e9c234899d7de8b2249e0f2e8caa6f58573600bf
vuong.do <vuong.do@alta.com.vn>
2021-12-14 11:01:51 +0700

    Fetch date use two form static and sever side

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Image from "next/image";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +30,20 @@ export default function Home() {
             Vuong Do Portfolio
           </a>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
